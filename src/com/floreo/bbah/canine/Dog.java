2a42fca6ad8d488a71754f6871778e3776518372
Bobby <bobby@floreolabs.org>
2020-04-20 17:07:28 -0400

    Adds canine impl

--- src/com/floreo/bbah/canine/Dog.java
+++ src/com/floreo/bbah/canine/Dog.java
@@ -1,4 +1,32 @@
 package com.floreo.bbah.canine;
 
-public class Dog {
+public class Dog implements Canine {
+
+    int weight;
+    boolean isDomesticated;
+
+    public Dog(int weight, boolean isDomesticated){
+        this.weight = weight;
+        this.isDomesticated = isDomesticated;
+    }
+
+    @Override
+    public boolean hasWinterCoat(int temp) {
+        return false;
+    }
+
+    @Override
+    public boolean isPeopleShy(boolean isFull) {
+        return false;
+    }
+
+    public boolean isBarking(boolean isStranger){
+        return isStranger;
+
+    }
+
+    public boolean isChasing(boolean isSquirrel){
+
+        return isSquirrel;
+    }
 }
